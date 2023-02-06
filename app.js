b3ca0e1c0dee001f42519f353bc30beeaaa1c5ca
incolorate <88613908+incolorate@users.noreply.github.com>
2023-02-06 10:10:59 +0200

    UI changed -game works -color picker does not work

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let SMALL = 16; 
+let SMALL = 16;
 
 let gameBoard = document.querySelector(".board");
 let buttons = document.querySelectorAll(".buttons");
@@ -12,24 +12,20 @@ function insertPixel(n) {
     let pixel = document.createElement("div");
     gameBoard.appendChild(pixel);
     pixel.classList.add("pixel");
-    pixel.addEventListener("mouseover", addColor)
+    pixel.addEventListener("mouseover", addColor);
   }
 }
 
+function coloring() {}
 
 // Check if mouse is clicked
-let mouseClicked = false
-function mouseUpdate () {
-  mouseClicked = !mouseClicked
+let mouseClicked = false;
+function mouseUpdate() {
+  mouseClicked = !mouseClicked;
 }
-document.addEventListener("mousedown", mouseUpdate)
-document.addEventListener("mouseup", mouseUpdate)
+document.addEventListener("mousedown", mouseUpdate);
+document.addEventListener("mouseup", mouseUpdate);
 
-function addColor () {
-  if (mouseClicked) {
-  this.classList.add("red")
-  }
-}
 // First delete the current board -> init new board
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -38,5 +34,13 @@ buttons.forEach((button) => {
   });
 });
 
-
 document.addEventListener("load", insertPixel(SMALL));
+
+let colorPick = document.querySelector(".color");
+
+function addColor() {
+  color = colorPick.value;
+  if (mouseClicked) {
+    this.style.backgroundColor = color;
+  }
+}
